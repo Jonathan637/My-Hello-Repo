d2b2e5f83d2f1ca35bed38bc45b292dd460bbf86
Jonathan637 <92557188+Jonathan637@users.noreply.github.com>
2021-10-22 01:08:23 +0000

    made updates

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-print("Hello World")
-print("Coding")
-print("global")
+import random
+lowNumber = input("enter a low number ")
+highNumber = input("enter a high number ")
+
+lives = 5
+storednumber = random.randint(int(lowNumber),int(highNumber))
+print("My random number generator")
+
+while lives > 0:
+  userguess =int( input("enter a number "))
+ 
+  if userguess > storednumber:
+    print("Your guess is too big!")
+    lives -= 1
+    print(lives)
+  if userguess < storednumber:
+    print("Your guess is too small!")
+    lives -= 1
+    print(lives)
+  if userguess == storednumber:
+    break  
+print(storednumber)  
+if lives == 0:
+  print("You lose")
+else:
+  print("You win")
+
+
+
+
+ 
